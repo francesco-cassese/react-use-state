64918d7d793952f052c490d8481e4e3f27aac48b
Francesco Cassese <francescocassese9@gmail.com>
2026-04-30 14:41:01 +0200

    feat: creata card iniziale con il map dei dati dell'array

---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+function Card(props) {
+    console.log(props.listLanguages)
+    const listaLinguaggiJsx = props.listLanguages.map(language => {
+        const { id, title, description } = language;
+        return (
+            <div key={id} className="card">
+                <div className="card-body">
+                    <button className="btn btn-primary">{title}</button>
+                    <p>{description}</p>
+                </div>
+            </div>
+        )
+    })
+    return (
+        <>{listaLinguaggiJsx}</>
+    )
+}
+export default Card
